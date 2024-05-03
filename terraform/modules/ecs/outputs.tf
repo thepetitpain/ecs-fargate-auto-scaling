243713f34dceca59edd7a5465a3dcca0698a4b31
Thomas PETETIN <thomas.petet1@gmail.com>
2024-05-03 15:32:52 +0200

    isolating task definitions and services in a module

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -1,11 +1,3 @@
 output "ecs_cluster" {
   value = aws_ecs_cluster.ecs_cluster
 }
-
-output "ecs_service" {
-  value = aws_ecs_service.ecs_service
-}
-
-output "ecs_task_definition" {
-  value = aws_ecs_task_definition.ecs_td
-}
